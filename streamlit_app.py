017210fdfb1dba69e840899a066e8440d9fbd45a
Wolverine-max <himanshunutron@gmail.com>
2024-11-26 19:09:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,7 @@ st.title('Sentiment Analysis App')
 st.write('This app is for sentiment analysis of social media data !')
 st.sidebar.title('Sentiment analysis of airlines')
 df1=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Twitter_Data.csv')
+if st.checkbox("Show Data"):
+    st.write(data.head(50))
+st.sidebar.subheader('Tweets Analyser')
+
