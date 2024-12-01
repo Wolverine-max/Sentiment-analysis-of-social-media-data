4e5475be30aae9df30b59f84823d9ee0e0f647a8
Wolverine-max <himanshunutron@gmail.com>
2024-12-01 18:52:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ st.title('Sentiment Analysis App')
 
 st.write('This app is for sentiment analysis of social media data !')
 st.sidebar.title('Sentiment analysis of Social Media Data')
-with st.expander('Dataset1'):
+with st.expander('Twitter Data'):
   st.write('**Raw data**')
   df1=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Twitter_Data.csv')
   df1
-with st.expander('Dataset1'):
+with st.expander('Reddit Data'):
   st.write('**Raw data**')
   df2=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Reddit_Data.csv')
   df2
