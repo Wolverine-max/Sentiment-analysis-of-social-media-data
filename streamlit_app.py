0a3dc36da8137605bca155805029ad07878f4462
Wolverine-max <himanshunutron@gmail.com>
2024-12-01 19:33:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ with st.expander('Reddit Data'):
   df2=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Reddit_Data.csv')
   df2.columns=['text','labels']
   st.dataframe(df2)
+with st.expander('Merged Data'):
 if not df1.empty and not df2.empty:
     st.write("**Merged Data**")
     merged_df = pd.concat([df1, df2], ignore_index=True)  # Concatenating the datasets
