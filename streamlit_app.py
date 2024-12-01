6ff0ca37c7ab52774e7a95a18b873f7009140296
Wolverine-max <himanshunutron@gmail.com>
2024-12-01 18:57:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,14 @@ with st.expander('Reddit Data'):
   df2
 st.sidebar.subheader('Data Analyser')
 tweets=st.sidebar.radio('Sentiment Type',('positive','negative','neutral'))
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
-st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(df1.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(df1.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(df1.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
 #selectbox + visualisation
 # An optional string to use as the unique key for the widget. If this is omitted, a key will be generated for the widget based on its content.
 ## Multiple widgets of the same type may not share the same key.
 select=st.sidebar.selectbox('Visualisation Of Tweets',['Histogram','Pie Chart'],key=1)
-sentiment=data['airline_sentiment'].value_counts()
+sentiment=df1['Sentiment Analysis'].value_counts()
 sentiment=pd.DataFrame({'Sentiment':sentiment.index,'Tweets':sentiment.values})
 st.markdown("###  Sentiment count")
 if select == "Histogram":
@@ -40,19 +40,19 @@ else:
 #slider
 st.sidebar.markdown('Time & Location of tweets')
 hr = st.sidebar.slider("Hour of the day", 0, 23)
-data['Date'] = pd.to_datetime(data['tweet_created'])
-hr_data = data[data['Date'].dt.hour == hr]
+df1['Date'] = pd.to_datetime(df1['tweet_created'])
+hr_df1 = df1[df1['Date'].dt.hour == hr]
 if not st.sidebar.checkbox("Hide", True, key='1'):
     st.markdown("### Location of the tweets based on the hour of the day")
     st.markdown("%i tweets during  %i:00 and %i:00" % (len(hr_data), hr, (hr+1)%24))
-    st.map(hr_data)
+    st.map(hr_df1)
 
 #multiselect
 st.sidebar.subheader("Airline tweets by sentiment")
 choice = st.sidebar.multiselect("Airlines", ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key = '0')  
 if len(choice)>0:
-    air_data=data[data.airline.isin(choice)]
+    air_df1=df1[df1.airline.isin(choice)]
     # facet_col = 'airline_sentiment'
-    fig1 = px.histogram(air_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600, width=800)
+    fig1 = px.histogram(air_df1, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600, width=800)
     st.plotly_chart(fig1)
 
