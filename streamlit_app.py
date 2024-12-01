c40e26e91762444c7af9115cbb6af50b322e0f68
Wolverine-max <himanshunutron@gmail.com>
2024-12-01 18:53:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,40 @@ with st.expander('Reddit Data'):
   df2=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Reddit_Data.csv')
   df2
 st.sidebar.subheader('Data Analyser')
+tweets=st.sidebar.radio('Sentiment Type',('positive','negative','neutral'))
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+#selectbox + visualisation
+# An optional string to use as the unique key for the widget. If this is omitted, a key will be generated for the widget based on its content.
+## Multiple widgets of the same type may not share the same key.
+select=st.sidebar.selectbox('Visualisation Of Tweets',['Histogram','Pie Chart'],key=1)
+sentiment=data['airline_sentiment'].value_counts()
+sentiment=pd.DataFrame({'Sentiment':sentiment.index,'Tweets':sentiment.values})
+st.markdown("###  Sentiment count")
+if select == "Histogram":
+        fig = px.bar(sentiment, x='Sentiment', y='Tweets', color = 'Tweets', height= 500)
+        st.plotly_chart(fig)
+else:
+        fig = px.pie(sentiment, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+#slider
+st.sidebar.markdown('Time & Location of tweets')
+hr = st.sidebar.slider("Hour of the day", 0, 23)
+data['Date'] = pd.to_datetime(data['tweet_created'])
+hr_data = data[data['Date'].dt.hour == hr]
+if not st.sidebar.checkbox("Hide", True, key='1'):
+    st.markdown("### Location of the tweets based on the hour of the day")
+    st.markdown("%i tweets during  %i:00 and %i:00" % (len(hr_data), hr, (hr+1)%24))
+    st.map(hr_data)
+
+#multiselect
+st.sidebar.subheader("Airline tweets by sentiment")
+choice = st.sidebar.multiselect("Airlines", ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key = '0')  
+if len(choice)>0:
+    air_data=data[data.airline.isin(choice)]
+    # facet_col = 'airline_sentiment'
+    fig1 = px.histogram(air_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600, width=800)
+    st.plotly_chart(fig1)
 
